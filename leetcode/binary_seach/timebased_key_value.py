e69ee12fc718be2b0e3a356d5528d6bb65918789
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-06-08 08:10:13 -0400

    Binary Search

---
+++ leetcode/binary_seach/timebased_key_value.py
@@ -0,0 +1,39 @@
+# Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+# Implement the TimeMap class:
+
+# TimeMap() Initializes the object of the data structure.
+# void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+# String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+# Example 1:
+
+# Input
+# ["TimeMap", "set", "get", "get", "set", "get", "get"]
+# [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+# Output
+# [null, null, "bar", "bar", null, "bar2", "bar2"]
+
+class TimeMap:
+
+    def __init__(self):
+        self.dic = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.dic:
+            self.dic[key] = []
+        self.dic[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.dic.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (l + r ) // 2
+            if values[mid][1] <= timestamp:
+                l = mid + 1
+                res = values[mid][0]
+            else:
+                r = mid - 1
+        return res
