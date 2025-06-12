86d172c04360fdc56b14de641bfef1e7a4094b88
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-06-12 06:57:26 -0400

    Binary Tree

---
+++ leetcode/trees/lowest_common_ancestor.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowest_common_ancestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                return root
